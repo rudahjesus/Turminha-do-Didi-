c4ccbcb3c98042312a42498a75b42d19bd8ad8a6
Wolfgang <161239309+w0lfg4ng@users.noreply.github.com>
2024-08-21 08:32:28 -0300

    Create teste_ultrassonico.ino
    betarum

---
+++ ultrasound_code/teste_ultrassonico.ino
@@ -0,0 +1,28 @@
+//CODE BY MORCEGOPOCILGA
+
+const int trigPin = 6;
+const int echoPin = 7;
+
+float duration, distance;
+
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distância: ");
+  Serial.println(distance);
+  delay(100);
+
+}
