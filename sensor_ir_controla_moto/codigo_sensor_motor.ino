72d85d56bf329d5a7a06c544c5fc022733ddc0b9
Wolfgang <161239309+w0lfg4ng@users.noreply.github.com>
2024-08-22 14:40:41 -0300

    Create codigo_sensor_motor.ino

---
+++ sensor_ir_controla_moto/codigo_sensor_motor.ino
@@ -0,0 +1,11 @@
+//CODE BY TURMINHA DO DIDI 😂🤣 MORCEGOPOCILGA
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
