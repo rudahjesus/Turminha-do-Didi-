3f4a60c8ac0c11cba07d7ba3367de8b4380f1db6
Wolfgang <161239309+w0lfg4ng@users.noreply.github.com>
2024-08-28 12:35:02 -0300

    Update sensor-ir-teste.ino
    teste no UNO

--- sensor-ir-teste/sensor-ir-teste.ino
+++ sensor-ir-teste/sensor-ir-teste.ino
@@ -1,36 +1,50 @@
-int PinsIr[5]={A0, A1, A2, A3, A4};
-int SAnalog[5]={0,0,0,0,0};
-//int Motor[4]{2, 3, 4, 5};
+const int irPins[5] = {5,6,9,10};
+int irSensorDigital[5] = {0,0,0,0};
 
-void setup() {
- 
- 
 
-  for (int i=0; i<5; i++){ 
-    pinMode(PinsIr[i], INPUT);
+void setup () {
+
+    /* Set-up IR sensor pins as input */
+  for (int i = 0; i <= 4; i++){
+    pinMode(irPins[i], INPUT);
   }
-Serial.begin(9600);
-  // for (int i=0; i<4; i++){
-  //   pinMode(Motor[i], OUTPUT);
-  // }
+
+  Serial.begin(9600);
+
 }
-void loop() {
-
-  scanA();
-
-  
-
+void loop (){
+    scanA();
 }
 
 void scanA(){
 
-  for ( int valorsensor = 0; valorsensor < 5;valorsensor++ ){
-    SAnalog[valorsensor] = analogRead(PinsIr[valorsensor]);
-    Serial.print(SAnalog[valorsensor]);
-    Serial.print("|");
+  for ( int count = 0; count < 5;count++ ){
+
+    irSensorDigital[count] = digitalRead(irPins[count]);
+/*
+  if(irSensorDigital[count] > 800 && irSensorDigital[count] < 1000){
+    Serial.print("Preto |");
+
   }
+  else if (irSensorDigital[count] > 0 && irSensorDigital[count] < 150){
+    Serial.print("Branco |");
+  }
+  else
+  Serial.print("VerdeBrat |"); */
+
+  Serial.print(irSensorDigital[0]);
+  Serial.print("|");
+  Serial.print(irSensorDigital[1]);
+  Serial.print("|");
+  Serial.print(irSensorDigital[2]);
+  Serial.print("|");
+  Serial.print(irSensorDigital[3]);
+  Serial.print("|");
+  Serial.print(irSensorDigital[4]);
+  Serial.print("|");
   Serial.println(".");
   delay (50); 
-}
-
   
+}
+ 
+} 
