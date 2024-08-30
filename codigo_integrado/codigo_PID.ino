85e7b4f71a632f3607c66aadce4da039dfd4aee9
Rudah S Jesus <156484678+rudahjesus@users.noreply.github.com>
2024-08-30 08:36:05 -0300

    Update codigo_PID.ino

--- codigo_integrado/codigo_PID.ino
+++ codigo_integrado/codigo_PID.ino
@@ -8,10 +8,11 @@ const int baseSpeed = 160; // Define a velocidade base dos motores, que será aj
 // PID
 int lastError = 0; // Armazena o último erro calculado, usado para o termo derivativo do PID
 float Kp = 55, Kd = 30, Ki = 0.00; // Constantes de ajuste do PID (Proporcional, Derivativo, Integral)
+int proportional, integral, derivative; // Variáveis PID
 
 //SENSORES
 QTRSensors qtr; // Cria um objeto da classe QTRSensors para gerenciar os sensores de refletância
-const uint8_t SensorCount = 5; // Define o número de sensores de refletância usados (neste caso, 5 sensores)
+const uint8_t SensorCount = 5; // Define o número de sensores de refletância usados
 uint16_t sensorValues[SensorCount]; // Array para armazenar os valores lidos pelos sensores
 
 //SETUP
@@ -38,14 +39,27 @@ void calibrateSensors() {
 
 void loop() {
   int error = 2000 - qtr.readLineBlack(sensorValues); // Calcula o erro, que é a diferença entre a posição desejada (2000) e a posição lida pelos sensores
-  int proportional = error; // O termo Proporcional é simplesmente o erro atual (erro * t)
-  int integral += error; // O termo Integral acumula o erro ao longo do tempo (Sum[erro] dt)
-  int derivative = error - lastError; // O termo Derivativo é a diferença entre o erro atual e o erro anterior (delta erro / delta t)
+  proportional = error; // O termo Proporcional é simplesmente o erro atual (erro * t)
+  integral += error; // O termo Integral acumula o erro ao longo do tempo (Ki∫e dt)
+  derivative = error - lastError; // O termo Derivativo é a diferença entre o erro atual e o erro anterior (Kd(Δe/Δt) ou de/dt)
   lastError = error; // Atualiza o último erro para o próximo cálculo do Derivativo
   
+  // S = ∑((Kp(et) + Ki∫e dt + Kd(de/dt))
   int PIDValue = (Kp * proportional) + (Ki * integral) + (Kd * derivative); // Calcula o valor de correção da velocidade usando o PID
-  int speedA = constrain(baseSpeed + PIDValue, 0, speedMax); // Ajusta a velocidade do motor A com base no cálculo do PID e limita a velocidade para o intervalo [0, speedMax]
-  int speedB = constrain(baseSpeed - PIDValue, 0, speedMax); // Ajusta a velocidade do motor B com base no cálculo do PID e limita a velocidade para o intervalo [0, speedMax]
+  bool noventa_graus = (sensorValues[0] < 100 || sensorValues[4] < 100); // 90 graus
+
+  //Limita velocidade com base na posição
+  int speedA, speedB;
+    if (noventa_graus) {
+      // Ajusta as velocidades para uma curva
+      speedA = constrain(baseSpeed - PIDValue, 0, speedMax); // Motor interno da curva mais lento
+      speedB = constrain(baseSpeed + PIDValue, 0, speedMax); // Motor externo da curva mais rápido
+    } else {
+      // Controle normal de linha reta
+      speedA = constrain(baseSpeed + PIDValue, 0, speedMax);
+      speedB = constrain(baseSpeed - PIDValue, 0, speedMax);
+    }
+
   driveMotors(speedA, speedB); // Envia as velocidades ajustadas para os motores para que o robô siga a linha
 }
 
@@ -55,4 +69,3 @@ void driveMotors(int posa, int posb) {
   analogWrite(motorB1, LOW); 
   analogWrite(motorB2, posb);   // Controla a direção e a velocidade dos motores A e B
 }
-
